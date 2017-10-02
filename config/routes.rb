abcdd857c63b32a798209ac0dcb856551cf1feb9
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-02 19:33:11 +0100

    did rails g andmade the file structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'countries/index'
+
+  get 'countries/show'
+
+  get 'countries/edit'
+
+  get 'countries/create'
+
+  get 'countries/new'
+
+  get 'countries/update'
+
+  get 'countries/destroy'
+
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/edit'
+
+  get 'movies/create'
+
+  get 'movies/new'
+
+  get 'movies/update'
+
+  get 'movies/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
